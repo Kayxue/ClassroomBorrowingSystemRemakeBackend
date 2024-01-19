ebc70f6021da51917481350d9e35d4c3a617d85c
Kayxue <s0990006@gmail.com>
2024-01-19 17:00:46 +0800

    2024/1/19 Finished Session-based Auth System

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [AuthModule, UserModule],
