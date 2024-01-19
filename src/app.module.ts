bc1a05ec5e187455b8d4215dbf704665a5f6e68e
Kayxue <s0990006@gmail.com>
2024-01-19 09:59:14 +0800

    2024/1/19 init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [PrismaModule.forRoot()],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
