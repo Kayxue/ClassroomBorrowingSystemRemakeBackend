05cfad666a2039ed44e66fc9d9010a4bea24fda5
Kayxue <s0990006@gmail.com>
2024-01-19 13:07:03 +0800

    2024/1/19 Modified Relations of modules, services and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, AuthService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
