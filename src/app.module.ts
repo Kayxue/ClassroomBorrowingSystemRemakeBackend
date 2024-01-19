15d34b157a3b1b927655af80120c3b6ddec55eb3
Kayxue <s0990006@gmail.com>
2024-01-19 12:20:27 +0800

    2024/1/19 Added some controllers, services, modules and PostgreSQL Schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PrismaModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
---
+++ src/user/user.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+
+@Module({
+  controllers: [UserController]
+})
+export class UserModule {}
