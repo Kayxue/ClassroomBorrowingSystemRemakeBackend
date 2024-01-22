fab0c0f36ae764481b6a1a3804d48d7d0f350763
Kayxue <s0990006@gmail.com>
2024-01-22 10:43:59 +0800

    2024/1/22 Add whether to get borrows of classroom to get classroom route

--- src/classroom/classroom.controller.ts
+++ src/classroom/classroom.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, ParseBoolPipe, Query } from '@nestjs/common';
 import { ClassroomService } from './classroom.service';
 
 @Controller('classroom')
 export class ClassroomController {
     public constructor(private classroomService: ClassroomService) { }
 
-    @Get("get")
-    public async getClassroom(@Query('id') id?: string) {
-        return this.classroomService.getClassroom(id);
+    @Get("getClassroom")
+    public async getClassroom(@Query('id') id?: string, @Query("borrows", new ParseBoolPipe({ optional: true })) borrows?: boolean) {
+        return this.classroomService.getClassroom(id, borrows);
     }
 }
--- src/classroom/classroom.service.ts
+++ src/classroom/classroom.service.ts
@@ -5,8 +5,8 @@ import { PrismaService } from '../prisma-service/prisma-service.service';
 export class ClassroomService {
     public constructor(private prismaService: PrismaService) { }
 
-    public async getClassroom(id?: string) {
-        if (id) return this.prismaService.classroomData.findUnique({ where: { id }, include: { borrowingDatas: true } });
-        return this.prismaService.classroomData.findMany({ include: { borrowingDatas: true } })
+    public async getClassroom(id?: string, borrows?: boolean) {
+        if (id) return this.prismaService.classroomData.findUnique({ where: { id }, include: { borrowingDatas: borrows ?? false } });
+        return this.prismaService.classroomData.findMany({ include: { borrowingDatas: borrows ?? false } })
     }
 }
