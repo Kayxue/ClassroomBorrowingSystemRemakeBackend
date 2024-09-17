7b769d07ca124ca9e7be4146cb5ed49ed5b37a19
Kayxue <s0990006@gmail.com>
2024-09-17 11:51:29 +0800

    2024/9/17 Fix cors error when sending api request from website

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import expressSession from "express-session"
-import passport from "passport"
+import expressSession from 'express-session';
+import passport from 'passport';
 import { secret } from './Config';
 
 async function bootstrap() {
   require('dotenv').config();
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  app.use(expressSession({
-    secret: secret,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: true, credentials: true, preflightContinue: false },
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.use(
+    expressSession({
+      secret: secret,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3001);
 }
 bootstrap();
